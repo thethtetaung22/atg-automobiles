6b64c3d4130297ca28d5ee77dbd52e485b9257f1
Thet <thet@tractive.com.my>
2021-12-12 14:05:07 +0630

    initial

---
+++ components/body/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/Body.module.scss';
+
+const Body = () => {
+    return (
+        <div className={styles.container}>
+            <h1> This is Body </h1>
+        </div>
+    )
+}
+
+export default Body;
---
+++ components/footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/Footer.module.scss';
+
+const Footer = () => {
+    return (
+        <div className={styles.container}>
+            Copyright ©︎ Digital .......... Corporation. All Rights Reserved.
+        </div>
+    )
+}
+
+export default Footer;
